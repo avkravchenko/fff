0c3bae766982e83014557abab37b24a5b69d4eed
Alexander <112536439+avkravchenko@users.noreply.github.com>
2023-07-17 21:29:14 +0400

    deploy

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,5 @@ module.exports = defineConfig({
 });
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/my-project/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/fff/" : "/",
 };
